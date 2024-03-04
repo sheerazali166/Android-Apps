7e37c31d584c796d1e00a4023be7e0b227b10dc7
sheerazali166 <sheerazali@656gmail.com>
2024-03-04 13:41:50 +0500

    initial commit

---
+++ app/src/main/java/com/example/birthdayapp/MainActivity.kt
@@ -0,0 +1,187 @@
+package com.example.birthdayapp
+
+import android.os.Bundle
+import android.os.Message
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.modifier.modifierLocalProvider
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.birthdayapp.ui.theme.BirthdayAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BirthdayAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                   // Greeting("James")
+//                    greetingText(
+//                        message = "Happy Birthday Sam!",
+//                        from = "From Emma",
+//                        modifier = Modifier.padding(8.dp)
+//                    )
+
+                    GreetingImage(
+                        message = getString(R.string.happy_birthday_sam),
+                        from = getString(R.string.signature_text)
+                    )
+
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) {
+
+    val image = painterResource(id = R.drawable.androidparty)
+
+    Box(modifier) {
+
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            alpha = 0.5F
+        )
+
+        greetingText(
+            message = message,
+            from = from,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp),
+
+        )
+
+    }
+
+}
+
+@Composable
+fun greetingText(message: String,from: String, modifier: Modifier = Modifier) {
+
+
+
+    Column(verticalArrangement = Arrangement.Center, modifier = modifier) {
+
+        Text(
+            text = message,
+            fontSize = 100.sp,
+            lineHeight = 116.sp,
+            textAlign = TextAlign.Center
+        )
+
+        Text(
+            text = from,
+            fontSize = 36.sp,
+            modifier = Modifier
+                .padding(16.dp)
+                .align(alignment = Alignment.CenterHorizontally)
+            
+        )
+    }
+
+//    Row {
+//
+//        Text(
+//            text = message,
+//            fontSize = 100.sp,
+//            lineHeight = 116.sp
+//        )
+//
+//        Text(
+//            text = from,
+//            fontSize = 36.sp
+//        )
+//    }
+
+
+//    Text(
+//        text = message,
+//        fontSize = 100.sp,
+//        lineHeight = 116.sp
+//    )
+//    Text(text = from, fontSize = 36.sp)
+
+//    Row(content = {
+//
+//        Text(text = "Some text")
+//        Text(text = "Some more text")
+//        Text(text = "Last text")
+//    })
+
+
+//    Row {
+//
+//            Text(text = "Some text")
+//            Text(text = "Some more text")
+//            Text(text = "Last text")
+//    }
+
+
+
+
+
+
+    // Don't copy.
+//    Row {
+//        Text(text = "First Column")
+//        Text(text = "Second Column")
+//    }
+}
+
+
+
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    BirthdayAppTheme {
+       // Greeting("Android")
+//        greetingText(message = "Happy Birthday Sam!", from = "From Emma")
+          GreetingImage(
+              message = stringResource(id = R.string.happy_birthday_sam),
+              from = stringResource(id = R.string.signature_text)
+          )
+
+
+    }
+}
